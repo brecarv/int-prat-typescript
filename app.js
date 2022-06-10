3084209c2079e5b0b3fef36e7d211783869d0e1c
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-06-09 23:59:09 -0300

    add a basic TS function to sum 2 numbers

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+"use strict";
 let button = document.getElementById("button");
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
-
-function somarNumeros(numero1, numero2) {
-  if (typeof numero1 === "number" && typeof numero2 === "number")
+function adicionarNumeros(numero1, numero2) {
+    let resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
-  else return Number(numero1) + Number(numero2);
 }
-
-button.addEventListener("click", () => {
-  console.log(somarNumeros(input1.value, input2.value));
-});
+let devePrintar = true;
+let frase;
+frase = "O valor é: ";
+if (button) {
+    button.addEventListener("click", () => {
+        console.log(adicionarNumeros(Number(input1 === null || input1 === void 0 ? void 0 : input1.value), Number(input2 === null || input2 === void 0 ? void 0 : input2.value)));
+    });
+}
